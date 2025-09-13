21e2d977543088f0ad88c4d59d7b2e1d1c1910bf
AlexKrait <167958169+AlexKrait@users.noreply.github.com>
2025-09-13 15:28:03 +0500

    Initial

---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] float _timer = 0f;
+    [SerializeField] float _spawnInterval = 2f;
+    [SerializeField] float _spawnHeight = 7f;
+    [SerializeField] GameObject _boxPrefab;
+    [SerializeField] Rigidbody2D _rb;
+    
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        _rb.gravityScale = 0.05f;
+        
+        if (_timer > _spawnInterval)
+        {
+            _timer = 0f;
+            float randomPositionX = Random.Range(-7f, 7f);
+            Vector3 spawnPosition = new Vector3(randomPositionX, _spawnHeight, 0f);
+            GameObject box = Instantiate(_boxPrefab, spawnPosition, Quaternion.identity); 
+        }
+    }
+}
